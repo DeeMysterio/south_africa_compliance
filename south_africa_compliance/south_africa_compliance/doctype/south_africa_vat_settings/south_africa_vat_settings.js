9d46ca7192958696c0d982342028bf676e355cf3
Diksha Jadhav <dikshajadhav11.dj@gmail.com>
2022-01-28 13:57:06 +0530

    add vat audit report

--- south_africa_compliance/south_africa_compliance/doctype/south_africa_vat_settings/south_africa_vat_settings.js
+++ south_africa_compliance/south_africa_compliance/doctype/south_africa_vat_settings/south_africa_vat_settings.js
@@ -2,7 +2,22 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('South Africa VAT Settings', {
-	// refresh: function(frm) {
-
-	// }
+	refresh: function(frm) {
+		frm.set_query("company", function() {
+			return {
+				filters: {
+					country: "South Africa",
+				}
+			};
+		});
+		frm.set_query("account", "vat_accounts", function() {
+			return {
+				filters: {
+					company: frm.doc.company,
+					account_type: "Tax",
+					is_group: 0
+				}
+			};
+		});
+	}
 });
