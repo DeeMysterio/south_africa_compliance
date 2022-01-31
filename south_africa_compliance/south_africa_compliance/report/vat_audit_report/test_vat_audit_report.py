fad0d2ae362d1c8dd0b150c63f19dd4e622f8a5d
Diksha Jadhav <dikshajadhav11.dj@gmail.com>
2022-01-31 18:17:36 +0530

    fix incorrect import

--- south_africa_compliance/south_africa_compliance/report/vat_audit_report/test_vat_audit_report.py
+++ south_africa_compliance/south_africa_compliance/report/vat_audit_report/test_vat_audit_report.py
@@ -10,7 +10,7 @@ from frappe.utils import today
 from erpnext.accounts.doctype.account.test_account import create_account
 from erpnext.accounts.doctype.purchase_invoice.test_purchase_invoice import make_purchase_invoice
 from erpnext.accounts.doctype.sales_invoice.test_sales_invoice import create_sales_invoice
-from erpnext.regional.report.vat_audit_report.vat_audit_report import execute
+from south_africa_compliance.south_africa_compliance.report.vat_audit_report.vat_audit_report import execute
 
 
 class TestVATAuditReport(TestCase):
